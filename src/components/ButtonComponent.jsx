d398dcbb9ddb02a97ece14fe639db9a00b918563
Rohan Pahwa <109075631+rohantez@users.noreply.github.com>
2023-05-31 21:59:30 +0530

    Updates

--- src/components/ButtonComponent.jsx
+++ src/components/ButtonComponent.jsx
@@ -15,20 +15,40 @@ export default function ButtonComponent() {
   const [timeRemaining, setTimeRemaining] = useState(null);
   const [loading, setLoading] = useState(false); // Loading state
 
+  const refreshState = async () => {
+    if (window.ethereum) {
+      const web3 = new Web3(window.ethereum);
+      const buttonGame = new web3.eth.Contract(ButtonGameABI, contractAddress);
+      const currentKing = await buttonGame.methods.currentKing().call();
+      const totalValueLocked = await buttonGame.methods.prizePool().call();
+      const roundEndsAt = await buttonGame.methods.roundEndsAt().call();
+      const isRoundActive = await buttonGame.methods.isRoundActive().call();
+
+      setCurrentKing(currentKing);
+      setTotalValueLocked(totalValueLocked);
+      setRoundEndsAt(roundEndsAt);
+      setIsRoundActive(isRoundActive);
+    }
+  };
+
+  useEffect(() => {
+    refreshState();
+  }, []);
+
   useEffect(() => {
     const interval = setInterval(() => {
       if (roundEndsAt) {
         const now = Date.now();
         const remaining = roundEndsAt * 1000 - now;
         if (remaining >= 0) {
+          const seconds = Math.floor((remaining / 1000) % 60);
           const minutes = Math.floor((remaining / 1000 / 60) % 60);
-          const hours = Math.floor((remaining / (1000 * 60 * 60)) % 24);
           setTimeRemaining(
-            (hours < 10 ? "0" : "") +
-              hours +
+            (minutes < 10 ? "0" : "") +
+              minutes +
               ":" +
-              (minutes < 10 ? "0" : "") +
-              minutes
+              (seconds < 10 ? "0" : "") +
+              seconds
           );
         } else {
           setTimeRemaining("00:00");
@@ -40,37 +60,28 @@ export default function ButtonComponent() {
     return () => clearInterval(interval);
   }, [roundEndsAt]);
 
-  useEffect(() => {
-    if (window.ethereum) {
-      const web3 = new Web3(window.ethereum);
-      const buttonGame = new web3.eth.Contract(ButtonGameABI, contractAddress);
-      buttonGame.methods.currentKing().call().then(setCurrentKing);
-      buttonGame.methods.prizePool().call().then(setTotalValueLocked);
-      buttonGame.methods.roundEndsAt().call().then(setRoundEndsAt);
-      buttonGame.methods.isRoundActive().call().then(setIsRoundActive);
-    }
-  }, []);
-
   const handlePlayOrStartButton = async () => {
     if (!loading) {
-      setLoading(true); // Start loading
+      setLoading(true);
       if (isRoundActive && timeRemaining !== "00:00") {
         await handlePlayOrStart(signer, true);
       } else if (!isRoundActive && timeRemaining === "00:00") {
         await handlePlayOrStart(signer, false);
       }
-      setLoading(false); // End loading
+      refreshState();
     }
+    setLoading(false);
   };
 
   const handleEndRoundButton = async () => {
     if (!loading) {
-      setLoading(true); // Start loading
+      setLoading(true);
       if (isRoundActive && timeRemaining === "00:00") {
         await handleEndRound(signer);
       }
-      setLoading(false); // End loading
+      refreshState();
     }
+    setLoading(false);
   };
   return (
     <div className="flex font-syne justify-between">
@@ -80,7 +91,7 @@ export default function ButtonComponent() {
             <div className="absolute inset-0 bg-green-400 ring-1 ring-black"></div>
             <div className="relative bg-[#fcfced] -inset-x-2.5 -inset-y-2.5 flex items-center justify-center border-4 border-transparent shadow-sm font-light text-black ring-1 ring-black">
               <div className="flex flex-col">
-                <div className="flex flex-row gap-56 px-4 py-6 justify-between">
+                <div className="flex flex-row gap-36 lg:gap-56 xl:gap-56 px-4 py-6 justify-between">
                   <div className="flex gap-3 flex-col">
                     <span className="text-l flex gap-2 font-medium">
                       <p className="font-semibold"> Current King: </p>
@@ -116,11 +127,15 @@ export default function ButtonComponent() {
                     <span className="text-l flex gap-2 font-medium">
                       <p className="font-semibold">Time Remaining:</p>
                       <p className="inline-block text-md font-light ">
-                        {isRoundActive
-                          ? timeRemaining
-                            ? timeRemaining
-                            : "Loading..."
-                          : "Round not active"}
+                        {isRoundActive ? (
+                          timeRemaining ? (
+                            timeRemaining
+                          ) : (
+                            <span className="loader"></span>
+                          )
+                        ) : (
+                          "Round not active"
+                        )}
                       </p>
                       <BiStopwatch className="h-5 w-5 mb-[0.5px] inline-block" />
                     </span>
--- src/components/MainView.jsx
+++ src/components/MainView.jsx
@@ -1,27 +1,24 @@
 import ButtonComponent from "./ButtonComponent";
 
 export default function MainViewComponent() {
-  
-
   return (
     <div className="h-[75vh] flex flex-col pt-16 items-center bg-purple-600 border-[2px] border-black rounded-t-3xl">
       <ButtonComponent />
-
       <div className="relative mt-12">
         <div className="absolute inset-0 bg-orange-400 ring-1 ring-black"></div>
         <div className="relative bg-[#fcfced] -inset-x-2.5 -inset-y-2.5 flex items-center justify-center border-4 border-transparent shadow-sm font-light text-black ring-1 ring-black">
           <div className="flex flex-col">
             <div
               id="desc"
-              className="px-4 py-10 text-black font-syne max-w-4xl text-3xl font-bold"
+              className="px-4 py-10 text-black font-syne max-w-4xl font-bold"
             >
               <div className="flex flex-col text-center">
-                <h1>How does it work ?</h1>
-                <p className="text-xl font-medium text-justify pt-6 ">
+                <h1 className="text-2xl">How does it work ?</h1>
+                <p className="text-l lg:text-xl xl:text-xl font-medium text-justify pt-6 ">
                   This is an experiment moderated by a smart contract deployed
                   on Arbitrum One chain. <br />
                   Whenever someone presses the button above, the contract will
-                  request user to deposit{" "} <br/>
+                  request user to deposit <br />
                   <span className="bg-yellow-300 text-black">
                     0.0005 ETH
                   </span>{" "}
